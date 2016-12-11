7aafeac3cc323bbf31831b5ce75878478fddc570
Brendan Sudol <brendan.sudol@gsa.gov>
2016-12-10 20:53:23 -0500

    tidy up test

--- test/components/Cartogram.test.js
+++ test/components/Cartogram.test.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { mount, shallow } from 'enzyme';
+import React from 'react'
+import { shallow } from 'enzyme'
 
-import Cartogram from '../../src/components/Cartogram';
+import Cartogram from '../../src/components/Cartogram'
 
 describe('Cartogram', () => {
   const cartogram = shallow(<Cartogram data={{'CA': 1}} color='red' />)
@@ -11,17 +11,17 @@ describe('Cartogram', () => {
   })
 
   it('displays "name_short" inside state square', () => {
-    const state = cartogram.find('[data-state="CA"]');
-    expect(state.text()).toBe('Calif.');
+    const state = cartogram.find('[data-state="CA"]')
+    expect(state.text()).toBe('Calif.')
   })
 
   it('makes passed-in states the highlighted color', () => {
-    const state = cartogram.find('[data-state="CA"] > div');
-    expect(state.prop('style').backgroundColor).toBe('red');
+    const state = cartogram.find('[data-state="CA"] > div')
+    expect(state.prop('style').backgroundColor).toBe('red')
   })
 
   it('makes non-highlighted states the default color', () => {
-    const state = cartogram.find('[data-state="NY"] > div');
-    expect(state.prop('style').backgroundColor).toBe('#eee');
+    const state = cartogram.find('[data-state="NY"] > div')
+    expect(state.prop('style').backgroundColor).toBe('#eee')
   })
 })
